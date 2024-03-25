2db7b0efe8356c7cac6ec2bf5169937d17deb5b9
Belllum <126370328+Belllum@users.noreply.github.com>
2024-03-25 20:09:32 +0800

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [backendData, setBackendData] = useState([{}]);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    fetch("/api")
+      .then((response) => response.json())
+      .then((data) => {
+        setBackendData(data);
+        setIsLoaded(true);
+      });
+
+    console.log(backendData.location.country);
+  }, [isLoaded]);
+  return (
+    <div>
+      {typeof backendData.current === null && backendData.locaition === null ? (
+        <p>Loading...</p>
+      ) : (
+        <p>{backendData.location.country}</p>
+      )}
+    </div>
+  );
+}
+
+export default App;
